4b301cd64b65ce90b36b576a5d6a6c224a575286
Ashwin Prasad <ashwin@onpar.in>
2023-03-25 01:56:00 +0530

    added new scraper message

--- scraper.go
+++ scraper.go
@@ -2,8 +2,16 @@ package shared_types
 
 const WORK_TYPE_SCRAPE_URL = "SCRAPE_URL"
 
-type ScrapeUrl struct {
+type ScrapeUrlMessage struct {
 	MessagingBase
 	Url   string
 	Flags string
+}
+
+func NewCreateScrapeUrlMessage(taskId uint, url, flags string) *ScrapeUrlMessage {
+	return &ScrapeUrlMessage{
+		*NewMessagingBase(taskId, WORK_TYPE_SCRAPE_URL),
+		url,
+		flags,
+	}
 }
