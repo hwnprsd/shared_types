11ca950c35bf73cec6e4e1be6e1a73e0bb78be8f
Ashwin Prasad <ashwin@onpar.in>
2023-03-28 15:15:54 +0530

    summary

--- scraper.go
+++ scraper.go
@@ -1,6 +1,7 @@
 package shared_types
 
 const WORK_TYPE_SCRAPE_URL = "SCRAPE_URL"
+const WORK_TYPE_SUMMARIZE_BLOG = "SCRAPE_URL"
 
 type ScrapeUrlMessage struct {
 	MessagingBase
@@ -13,5 +14,17 @@ func NewCreateScrapeUrlMessage(taskId uint, url, flags string) *ScrapeUrlMessage
 		*NewMessagingBase(taskId, WORK_TYPE_SCRAPE_URL),
 		url,
 		flags,
+	}
+}
+
+type SummarizeBlogMessage struct {
+	MessagingBase
+	ScrapeData string
+}
+
+func NewSummarizeBlogMessage(taskId uint, data string) *SummarizeBlogMessage {
+	return &SummarizeBlogMessage{
+		*NewMessagingBase(taskId, WORK_TYPE_SUMMARIZE_BLOG),
+		data,
 	}
 }
