fa2877c917325a99a4f84a3bddbb3815fdd1d79c
Ashwin Prasad <ashwin@onpar.in>
2023-04-17 14:30:37 +0530

    parse bytes

--- pdf.go
+++ pdf.go
@@ -1,6 +1,7 @@
 package shared_types
 
 const WORK_TYPE_PDF_PARSE_CV = "PDF_PARSE_CV"
+const WORK_TYPE_PDF_PARSE_CV_BYTES = "PDF_PARSE_CV_BYTES"
 
 type PdfParseCVMessage struct {
 	MessagingBase
@@ -11,5 +12,17 @@ func NewPdfParseMessage(taskId uint, url string) *PdfParseCVMessage {
 	return &PdfParseCVMessage{
 		*NewMessagingBase(taskId, WORK_TYPE_PDF_PARSE_CV),
 		url,
+	}
+}
+
+type PdfParseCVBytesMessage struct {
+	MessagingBase
+	b []byte
+}
+
+func NewPdfParseBytesMessage(taskId uint, b []byte) *PdfParseCVBytesMessage {
+	return &PdfParseCVBytesMessage{
+		*NewMessagingBase(taskId, WORK_TYPE_PDF_PARSE_CV_BYTES),
+		b,
 	}
 }
